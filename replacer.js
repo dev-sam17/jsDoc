375bef5d4b5aecf631503b5d7f4a7f733f95be54
Saurav Kumar <codewizard.saurav@gmail.com>
2024-07-04 18:42:26 +0530

    Create replacer.js

---
+++ replacer.js
@@ -0,0 +1,36 @@
+const fs = require('node:fs/promises');
+
+const FILENAME = 'server.js'
+
+const readScript = async () => {
+    const textData = await fs.readFile(FILENAME, 'utf8')
+    const lines = textData.split('\n');
+
+    const newLines = lines.map((line, i) => {
+        if (line.includes('http://') || line.includes('racer.setUrl')) {
+            // console.log(line);
+
+            if (line.startsWith('//')) {
+                return line
+            }
+
+            return '// ' + line;
+        } else if (line.includes('configFilePath')) {
+            if (lines[i + 1].includes('logPath')) {
+
+                return line;
+            }
+            return line + '\n' + "\t\tlogPath: './logs'"
+        }
+
+        return line
+    })
+
+    // console.log(newLines);
+
+    await fs.writeFile(FILENAME, newLines.join('\n'))
+    //    console.log(JSON.stringify(newLines))
+}
+
+readScript();
+
