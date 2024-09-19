7bbc006da672f06c4ccfc3ca81be3cd18fec2fae
Saurav Kumar <dev_sam17@protonmail.com>
2024-09-19 19:15:19 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,21 @@
+mkdir grpc_test
+cd grpc_test
+
+curl -Ss "https://raw.githubusercontent.com/dev-sam17/jsDoc/main/tester.zip" > "tester.zip"
+
+unzip tester.zip
+mv npmrc .npmrc
+
+npm i
+
+cd node_modules/@reconvert/micoservice-clients/
+rm -rf *
+rm -rf .*
+
+
+git clone "" .
+git checkout ${1:-development}
+
+cd ../../..
+
+node ms-client-edit.js
