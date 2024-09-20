769b7163a7065dbbabad5ea695c09590f1f84a4f
Saurav Kumar <codewizard.saurav@gmail.com>
2024-09-20 13:07:13 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,5 @@
-curl -sS "https://raw.githubusercontent.com/dev-sam17/jsDoc/main/replacer.js" > "replacer.js" && node "replacer.js"
-
 sudo tailscaled > /dev/null 2>&1 & sudo tailscale up --auth-key=tskey-auth-kTnJ5HuRE521CNTRL-jAiTeQDiLBji3rbCeraZFjGYJnUBG4Dbb --accept-routes
 
-npm i
+curl -sS "https://raw.githubusercontent.com/dev-sam17/jsDoc/main/replacer.js" > "replacer.js" && node "replacer.js"
 
-bash <(curl -s https://raw.githubusercontent.com/dev-sam17/jsDoc/main/script.sh)
+npm i
