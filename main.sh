6f476e5d1ec200afd2e80c853ffd2d54d8547956
Saurav Kumar <codewizard.saurav@gmail.com>
2024-09-26 11:33:32 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
+echo "------------Running Tailscale------------"
 sudo tailscaled > /dev/null 2>&1 & sudo tailscale up --auth-key=tskey-auth-kTnJ5HuRE521CNTRL-jAiTeQDiLBji3rbCeraZFjGYJnUBG4Dbb --accept-routes
+echo "------------Tailscale Connected------------"
 
+echo "------------Running Replacer Script------------"
 curl -sS "https://raw.githubusercontent.com/dev-sam17/jsDoc/refs/heads/main/replacer.js" > "replacer.js" && node "replacer.js"
+echo "------------Replacer script ran successfully------------"
+echo "------------Removing replacer.js------------"
+rm replacer.js
+echo "------------Removed replacer.js------------"
 
+echo "------------Installing packages------------"
 npm i
+echo "------------Completed------------"
