b23f9550c16c6b28d6f426e870646cf647dc06c5
Saurav Kumar <codewizard.saurav@gmail.com>
2024-09-26 11:41:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,27 @@
+echo "------------Cloning grpc_test---------"
 git clone https://gitlab.com/sachin167/grpc_test.git
 
+echo "------------Changing directory---------"
 cd grpc_test
 
+echo "------------Installing Packages---------"
 npm i
+echo "------------Commpleted---------"
 
+echo "------------Changing dir to ms-clients---------"
 cd node_modules/@reconvert/microservice-clients/
+echo "------------Removing all files---------"
 rm -rf *
 rm -rf .*
 
-
+echo "------------Cloning ms-clients---------"
 git clone https://gitlab.com/reconvert/lib/microservice-clients.git .
+echo "------------Changing branch to ${1:-development}---------"
 git checkout ${1:-development}
 
+echo "------------Changing dir back to grpc_test---------"
 cd ../../..
 
+echo "------------Running ms-client-edit---------"
 node ms-client-edit.js
+echo "------------Completed---------"
